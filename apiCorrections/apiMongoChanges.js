3e79ecd91bac3ce2944c0e09dfee1c0b268897fb
PabMatMar <pabmatmar@gmail.com>
2023-07-12 21:38:32 +0200

    add/ script file to correct one or several fields within the objects of the movies collection (mongo)

---
+++ apiCorrections/apiMongoChanges.js
@@ -0,0 +1,28 @@
+const Movies = require('../models/moviesMongo');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const db = process.env.DB_URL_ATLAS;
+
+const changesFieldInMovies = async () => {
+    try {
+        const movies = await Movies.find({ Movies }, { _id: 0, __v: 0 });
+        for (let i = 0; i < movies.length; i++) {
+            //Introduzco el codigo para cambiar el campo que deseo
+            console.log("ENTER THE CODE TO CHANGE A FIELD IN MONGO!!");
+        };
+        await Movies.deleteMany({});
+        await Movies.insertMany(movies);
+    } catch (err) {
+        console.log(err.message);
+    };
+};
+
+mongoose
+    .connect(db)
+    .then(() => console.log('mongodb connection success'))
+    .catch((error) => console.log(error));
+
+changesFieldInMovies().then(() => {
+    mongoose.connection.close();
+    console.log('closed connection');
+});
